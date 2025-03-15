2ec46d8cec04624d6141b50cfe2ffed9c46bac53
Duckhanhh <Duckhanhh>
2025-03-15 22:05:47 +0700

    base project

---
+++ backend/abcbook/src/test/java/org/example/abcbook/AbcbookApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.abcbook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbcbookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
