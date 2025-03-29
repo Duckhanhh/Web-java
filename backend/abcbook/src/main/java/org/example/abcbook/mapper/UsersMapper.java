ec69ca7120b9bf7b1c5f5af34c852a54741265d8
Duckhanhh <Duckhanhh>
2025-03-29 12:31:02 +0700

    base

---
+++ backend/abcbook/src/main/java/org/example/abcbook/mapper/UsersMapper.java
@@ -0,0 +1,10 @@
+package org.example.abcbook.mapper;
+
+import org.example.abcbook.dto.request.UserRequest;
+import org.example.abcbook.model.Users;
+import org.mapstruct.Mapper;
+
+@Mapper(componentModel = "spring")
+public interface UsersMapper {
+    Users userRequestToUser(UserRequest userRequest);
+}
---
+++ backend/abcbook/src/main/java/org/example/abcbook/service/UsersService.java
@@ -0,0 +1,10 @@
+package org.example.abcbook.service;
+
+import org.example.abcbook.dto.request.UserRequest;
+import org.example.abcbook.model.Users;
+
+public interface UsersService {
+    Users getUserById(Long id);
+
+    void createUser(UserRequest userData);
+}
