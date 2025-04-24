b623e24a003845cf129a988c4aabab4ca94a509f
Duckhanhh <Duckhanhh>
2025-04-24 22:50:31 +0700

    fix loi lia

---
+++ Service/src/main/java/org/example/abcbook/dto/response/AuthenticationResponse.java
@@ -0,0 +1,17 @@
+package org.example.abcbook.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class AuthenticationResponse {
+    private String token;
+    private boolean authenticated;
+}
--- Service/src/main/java/org/example/abcbook/service/UsersService.java
+++ Service/src/main/java/org/example/abcbook/service/UsersService.java
@@ -1,16 +1,57 @@
 package org.example.abcbook.service;
 
 import org.example.abcbook.dto.request.UserRequest;
+
 import org.example.abcbook.dto.response.UserResponse;
 import org.example.abcbook.exception.AppException;
+import org.example.abcbook.mapper.UsersMapper;
 import org.example.abcbook.model.Users;
+import org.example.abcbook.repository.UsersRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 
-public interface UsersService {
-    Users getUserById(Long id);
+@Service
+public class UsersService {
+    @Autowired
+    private UsersRepository usersRepository;
+    @Autowired
+    private UsersMapper usersMapper;
+    
+    public Users getUserById(Long id) {
+        return usersRepository.findById(id).orElse(null);
+    }
 
-    void createUser(UserRequest userData) throws AppException;
+    @PreAuthorize("hasRole('ADMIN')")
+    public List<UserResponse> getAllUsers() {
+        return usersRepository.findAll().stream().map(usersMapper::toUserResponse).toList();
+    }
 
-    List<UserResponse> getAllUsers();
+    public void createUser(UserRequest userData) throws AppException {
+        if (userData == null) {
+            throw new AppException("CU00001", "create.user.data.empty");
+        }
+
+        if (usersRepository.existsByEmail(userData.getEmail())) {
+            throw new AppException("CU00002", "create.user.email.existed");
+        }
+
+        if (usersRepository.existsByPhoneNumber(userData.getPhoneNumber())) {
+            throw new AppException("CU00003", "create.user.phoneNumber.existed");
+        }
+
+        //Encode password
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder(10);
+        String encodedPassword = passwordEncoder.encode(userData.getPassword());
+        userData.setPassword(encodedPassword);
+
+        Users user = usersMapper.userRequestToUser(userData);
+        user.setRegistrationDate(new Date());
+        usersRepository.save(user);
+    }
 }
