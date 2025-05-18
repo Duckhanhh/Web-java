ff1b77a9b37985194627b06c3d3fd2d72510d2c6
Duckhanhh <Duckhanhh>
2025-05-18 19:36:03 +0700

    quan ly sach

--- Service/src/main/java/org/bpm/abcbook/service/BooksService.java
+++ Service/src/main/java/org/bpm/abcbook/service/BooksService.java
@@ -1,32 +1,17 @@
 package org.bpm.abcbook.service;
 
-import org.bpm.abcbook.exception.AppException;
 import org.bpm.abcbook.model.Books;
-import org.bpm.abcbook.repository.BooksRepo;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Date;
 import java.util.List;
 
-@Service
-public class BooksService {
-    private static final Logger logger = LoggerFactory.getLogger(BooksService.class);
-
-    @Autowired
-    private BooksRepo booksRepo;
-
+public interface BooksService {
     /**
      * Ham lay tat ca sach
      *
      * @return
      */
-    public List<Books> findAllBook() throws Exception {
-        return booksRepo.findAll();
-    }
+    List<Books> findAllBook() throws Exception;
 
     /**
      * Ham tim kiem tat ca sach
@@ -45,76 +30,16 @@ public class BooksService {
      * @param toAddDate
      * @return
      */
-    public List<Books> findAll(String title, Long fromPrice, Long toPrice, double fromRating, double toRating, String publisher,
-                               Long bookStatus, Long bookFormat, String author, String category, Date fromAddDate, Date toAddDate) throws Exception {
-        return booksRepo.findAll(title, fromPrice, toPrice, fromRating, toRating, publisher, bookStatus, bookFormat, author, category, fromAddDate, toAddDate);
-    }
+    List<Books> findAll(String title, Long fromPrice, Long toPrice, double fromRating, double toRating, String publisher,
+                        Long bookStatus, Long bookFormat, String author, String category, Date fromAddDate, Date toAddDate) throws Exception;
 
-    public void deleteBook(Long id) throws Exception {
-        booksRepo.deleteBook(id);
-    }
+    void deleteBook(Long id) throws Exception;
 
-    /**
-     * Cap nhat trang thai sach ve khong hieu luc
-     *
-     * @param book
-     * @throws Exception
-     */
-    @Transactional(rollbackFor = Exception.class)
-    public void updateBook(Books book) throws Exception {
-        if (book == null) {
-            throw new AppException("UB00001", "book.management.update.book.empty");
-        }
+    void updateBook(Books book) throws Exception;
 
-        //Kiem tra xem co ton tai hay khong
-        if (!booksRepo.existsById(book.getId())) {
-            throw new AppException("UB00001", "book.management.update.book.not.exist");
-        }
+    List<Books> findBookInStock(Long bookId, Date fromDate, Date toDate) throws Exception;
 
-        booksRepo.save(book);
-    }
+    void insertBook(Books book) throws Exception;
 
-    /**
-     * Ham lay sach trong kho
-     *
-     * @param bookId
-     * @param fromDate
-     * @param toDate
-     * @return
-     * @throws Exception
-     */
-    public List<Books> findBookInStock(Long bookId, Date fromDate, Date toDate) throws Exception {
-        return booksRepo.findBookInStock(bookId, fromDate, toDate);
-    }
-
-    /**
-     * Ham them sach
-     *
-     * @param book
-     * @throws Exception
-     */
-    @Transactional(rollbackFor = Exception.class)
-    public void insertBook(Books book) throws Exception {
-        if (book == null) {
-            throw new AppException("IB00001", "book.management.insert.book.empty");
-        }
-
-        //Kiem tra xem sach da ton tai hay chua
-        if (booksRepo.existsByTitle(book.getTitle())) {
-            throw new AppException("IB00001", "book.management.insert.book.exist");
-        }
-
-        book.setBookStatus(Books.BOOK_STATUS_ACTIVE);
-        book.setBookFormat(Books.BOOK_FORMAT_HARDCOVER);
-        book.setAddDate(new Date());
-
-        booksRepo.save(book);
-    }
-
-    public void deleteListBook(List<Long> idList) throws Exception {
-        if (idList == null || idList.isEmpty()) {
-            return;
-        }
-        booksRepo.deleteListBook(idList);
-    }
+    void deleteListBook(List<Long> idList) throws Exception;
 }
---
+++ Service/src/main/java/org/bpm/abcbook/service/BooksServiceImpl.java
@@ -0,0 +1,128 @@
+package org.bpm.abcbook.service;
+
+import org.bpm.abcbook.exception.AppException;
+import org.bpm.abcbook.model.Books;
+import org.bpm.abcbook.repository.BooksRepo;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Date;
+import java.util.List;
+
+@Service
+public class BooksServiceImpl implements BooksService {
+    private static final Logger logger = LoggerFactory.getLogger(BooksServiceImpl.class);
+
+    @Autowired
+    private BooksRepo booksRepo;
+
+    /**
+     * Ham lay tat ca sach
+     *
+     * @return
+     */
+    @Override
+    public List<Books> findAllBook() throws Exception {
+        return booksRepo.findAll();
+    }
+
+    /**
+     * Ham tim kiem tat ca sach
+     *
+     * @param title
+     * @param fromPrice
+     * @param toPrice
+     * @param fromRating
+     * @param toRating
+     * @param publisher
+     * @param bookStatus
+     * @param bookFormat
+     * @param author
+     * @param category
+     * @param fromAddDate
+     * @param toAddDate
+     * @return
+     */
+    @Override
+    public List<Books> findAll(String title, Long fromPrice, Long toPrice, double fromRating, double toRating, String publisher,
+                               Long bookStatus, Long bookFormat, String author, String category, Date fromAddDate, Date toAddDate) throws Exception {
+        return booksRepo.findAll(title, fromPrice, toPrice, fromRating, toRating, publisher, bookStatus, bookFormat, author, category, fromAddDate, toAddDate);
+    }
+
+    /**
+     * Ham xoa sach
+     *
+     * @param id
+     * @throws Exception
+     */
+    @Override
+    @Transactional(rollbackFor = Exception.class)
+    public void deleteBook(Long id) throws Exception {
+        booksRepo.deleteBook(id);
+    }
+
+    @Override
+    @Transactional(rollbackFor = Exception.class)
+    public void updateBook(Books book) throws Exception {
+        if (book == null) {
+            throw new AppException("UB00001", "book.management.update.book.empty");
+        }
+
+        //Kiem tra xem co ton tai hay khong
+        if (!booksRepo.existsById(book.getId())) {
+            throw new AppException("UB00001", "book.management.update.book.not.exist");
+        }
+
+        booksRepo.save(book);
+    }
+
+    /**
+     * Ham lay sach trong kho
+     *
+     * @param bookId
+     * @param fromDate
+     * @param toDate
+     * @return
+     * @throws Exception
+     */
+    @Override
+    public List<Books> findBookInStock(Long bookId, Date fromDate, Date toDate) throws Exception {
+        return booksRepo.findBookInStock(bookId, fromDate, toDate);
+    }
+
+    /**
+     * Ham them sach
+     *
+     * @param book
+     * @throws Exception
+     */
+    @Override
+    @Transactional(rollbackFor = Exception.class)
+    public void insertBook(Books book) throws Exception {
+        if (book == null) {
+            throw new AppException("IB00001", "book.management.insert.book.empty");
+        }
+
+        //Kiem tra xem sach da ton tai hay chua
+        if (booksRepo.existsByTitle(book.getTitle())) {
+            throw new AppException("IB00001", "book.management.insert.book.exist");
+        }
+
+        book.setBookStatus(Books.BOOK_STATUS_ACTIVE);
+        book.setBookFormat(Books.BOOK_FORMAT_HARDCOVER);
+        book.setAddDate(new Date());
+
+        booksRepo.save(book);
+    }
+
+    @Override
+    public void deleteListBook(List<Long> idList) throws Exception {
+        if (idList == null || idList.isEmpty()) {
+            return;
+        }
+        booksRepo.deleteListBook(idList);
+    }
+}
